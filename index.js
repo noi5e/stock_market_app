201fe945d13b1817233a3752a8404516d2cee9d8
noi5e <will.gutierrez@gmail.com>
2018-04-23 11:40:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/stock_market_app');
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost:27017/stock_market_app');
+mongoose.connect(process.env.MONGODB_URI);
 
 // look for static files
 app.use(express.static('./server/static'));
