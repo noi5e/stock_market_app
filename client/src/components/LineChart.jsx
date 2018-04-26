375fb61249aa1d615d95c87f766bbe700204c7a2
noi5e <will.gutierrez@gmail.com>
2018-04-25 18:38:47 -0700

    Update LineChart.jsx

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -14,7 +14,6 @@ class LineChart extends React.Component {
 	constructor(props) {
 		super(props);
 		this.renderOrUpdateChart = this.renderOrUpdateChart.bind(this);
-		this.getMouseOverData = this.getMouseOverData.bind(this);
 	}
 
 	componentDidMount() {
