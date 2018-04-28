4a21e66a7767b0cbbe0dc55a2bb3fa43d594f531
noi5e <will.gutierrez@gmail.com>
2018-04-27 17:45:43 -0700

    Update Tooltip.jsx

--- client/src/components/Tooltip.jsx
+++ client/src/components/Tooltip.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Tooltip extends React.Component {
-  
-  
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return <div id='tooltip'>
+		       </div>;
+	}
 }
+
+Tooltip.propTypes = {
+	tooltipData: PropTypes.object.isRequired
+}
+
+export default Tooltip;
