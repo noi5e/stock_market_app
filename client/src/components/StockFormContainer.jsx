302b1d52b06936d7968cc16896663a7aa3036bc7
Will Gutierrez <will.gutierrez@gmail.com>
2018-05-16 11:54:00 -0700

    oops, forgot to toggle ws to wss

--- client/src/components/StockFormContainer.jsx
+++ client/src/components/StockFormContainer.jsx
@@ -51,7 +51,7 @@ class StockFormContainer extends React.Component {
 	}
 
 	componentDidMount() {
-		this.socket = new WebSocket('ws://' + window.location.host);
+		this.socket = new WebSocket('wss://' + window.location.host);
 
 		this.socket.addEventListener('open', (e) => {
 			this.socket.send('New client opened up a socket!', (error) => {
